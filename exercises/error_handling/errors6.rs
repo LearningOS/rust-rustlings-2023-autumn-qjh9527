8f0ed753578d1a8756eb556f35b11cf537446ef5
QJH <2247829280@qq.com>
2024-03-25 09:24:26 +0800

    2024年3月22日

--- exercises/error_handling/errors6.rs
+++ exercises/error_handling/errors6.rs
@@ -9,9 +9,21 @@
 // Execute `rustlings hint errors6` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+/*
+
+1. ParsePosNonzeroError： 解析非零正整数时可能出现的错误
+	1. 解析**整数**时出现错误 (`ParseInt`)
+	2. 由**解析出的整数**生成**正整数**时出现错误 (`Creation`)
+2. CreationError： 创建正整数时可能出现的错误情况，包括负数和零
+3. 函数 `parse_pos_nonzero` ：解析字符串并转换为非零正整数
+	1. 尝试将字符串解析为整数类型 `i64`
+		1. 如果解析失败，则将 `ParseIntError` 转换为 `ParsePosNonzeroError` 类型，并返回
+		2. 如果解析成功，则调用 `PositiveNonzeroInteger::new` 函数创建非零正整数
+			1. 如果创建失败，则将 `CreationError` 转换为 `ParsePosNonzeroError` 类型，并返回。
+*/
 
 use std::num::ParseIntError;
+
 
 // This is a custom error type that we will be using in `parse_pos_nonzero()`.
 #[derive(PartialEq, Debug)]
@@ -25,13 +37,15 @@ impl ParsePosNonzeroError {
         ParsePosNonzeroError::Creation(err)
     }
     // TODO: add another error conversion function here.
-    // fn from_parseint...
+    fn from_parseint(err: ParseIntError) -> ParsePosNonzeroError {
+        ParsePosNonzeroError::ParseInt(err)
+    }
 }
 
 fn parse_pos_nonzero(s: &str) -> Result<PositiveNonzeroInteger, ParsePosNonzeroError> {
     // TODO: change this to return an appropriate error instead of panicking
     // when `parse()` returns an error.
-    let x: i64 = s.parse().unwrap();
+    let x: i64 = s.parse().map_err(ParsePosNonzeroError::from_parseint)?;
     PositiveNonzeroInteger::new(x).map_err(ParsePosNonzeroError::from_creation)
 }
 
