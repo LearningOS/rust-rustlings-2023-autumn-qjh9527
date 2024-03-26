d2ea8c31594208656fcfde6bb9074f1ffc3f36be
QJH <2247829280@qq.com>
2024-03-26 10:40:24 +0800

    lifetimes

--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -5,11 +5,12 @@
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
-struct Book {
-    author: &str,
-    title: &str,
+// 定义引用结构体时，需要为其每一个引用添加生命周期注解
+// 为了确保引用结构体的实例在使用引用时是安全的，需要使用生命周期注解来表明引用结构体的字段与引用结构体实例之间的关系。
+struct Book<'a> {
+    author: &'a str,
+    title: &'a str,
 }
 
 fn main() {
