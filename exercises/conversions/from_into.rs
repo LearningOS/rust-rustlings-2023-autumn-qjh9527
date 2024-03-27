27978255fece0e8c0aa4e2fee88fa87484a102fa
QJH <2247829280@qq.com>
2024-03-27 10:51:21 +0800

    conversions

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+
+        let name_age: Vec<&str> = s.split(',').collect();
+        if name_age.len() != 2 {
+            return Person::default();
+        }
+
+        let name = name_age[0].to_string();
+        if name.is_empty(){
+            return Person::default();
+        }
+        match name_age[1].parse::<usize>() {
+            Ok(age) => Person {name, age },
+            Err(_) => Person::default()
+         }
+
     }
 }
 
